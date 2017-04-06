0636ee93640857b8735c2df20a8d37b1ee0c5a8b
Eric Zhao <zhao.yichao15@gmail.com>
2017-04-06 21:02:29 +1000

    add watcher

---
+++ Ice.Core.Start/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ice.Core.Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
